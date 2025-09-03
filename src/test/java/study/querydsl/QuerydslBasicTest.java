1b8eb339e05e631092e9a01c1b0a30479d550de3
xogur <snsk656@naver.com>
2025-09-03 19:27:18 +0900

    9 3 5

--- src/test/java/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -4,6 +4,8 @@ import com.querydsl.core.QueryResults;
 import com.querydsl.core.Tuple;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.PersistenceUnit;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -237,6 +239,40 @@ public class QuerydslBasicTest {
         for (Tuple tuple : result) {
             System.out.println ("tuple = " + tuple);
         }
+    }
+
+    @PersistenceUnit
+    EntityManagerFactory emf;
+
+    @Test
+    public void fetchJoinNo() {
+        em.flush ();
+        em.clear ();
+
+        Member findMember = queryFactory
+                .selectFrom (member)
+                .where (member.username.eq ("member1"))
+                .fetchOne ();
+
+        boolean loaded = emf.getPersistenceUnitUtil ().isLoaded (findMember.getTeam ());
+        assertThat (loaded).as ("페치 조인 미적용").isFalse ();
+
+    }
+
+    @Test
+    public void fetchJoinUse() {
+        em.flush ();
+        em.clear ();
+
+        Member findMember = queryFactory
+                .selectFrom (member)
+                .join (member.team, team).fetchJoin ()
+                .where (member.username.eq ("member1"))
+                .fetchOne ();
+
+        boolean loaded = emf.getPersistenceUnitUtil ().isLoaded (findMember.getTeam ());
+        assertThat (loaded).as ("페치 조인 미적용").isTrue ();
+
     }
 
 
