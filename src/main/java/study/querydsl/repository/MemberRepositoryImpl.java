4d0d7a53e816ba476e36e233e61106b558af0f6d
xogur <snsk656@naver.com>
2025-09-06 15:37:56 +0900

    9 6 4

--- src/main/java/study/querydsl/repository/MemberRepositoryImpl.java
+++ src/main/java/study/querydsl/repository/MemberRepositoryImpl.java
@@ -2,14 +2,17 @@ package study.querydsl.repository;
 
 import com.querydsl.core.QueryResults;
 import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.support.PageableExecutionUtils;
 import study.querydsl.dto.MemberSearchCondition;
 import study.querydsl.dto.MemberTeamDto;
 import study.querydsl.dto.QMemberTeamDto;
+import study.querydsl.entity.Member;
 
 import java.util.List;
 
@@ -99,7 +102,7 @@ public class MemberRepositoryImpl implements MemberRepositoryCustom {
                 .fetch ();
 
 
-        long total = queryFactory
+        JPAQuery<Member> countQuery = queryFactory
                 .select (member)
                 .from (member)
                 .leftJoin (member.team, team)
@@ -108,10 +111,11 @@ public class MemberRepositoryImpl implements MemberRepositoryCustom {
                         teanNameEq (condition.getTeamName ()),
                         ageGoe (condition.getAgeGoe ()),
                         ageLoe (condition.getAgeLoe ())
-                )
-                .fetchCount ();
+                );
 
-        return new PageImpl<> (content, pageable, total);
+        return PageableExecutionUtils.getPage (content, pageable, () -> countQuery.fetchCount () );
+
+//        return new PageImpl<> (content, pageable, total);
 
     }
 
